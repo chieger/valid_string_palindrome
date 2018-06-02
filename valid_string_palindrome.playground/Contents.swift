9fcba8555d2f25ea8cbb0ec05aa6319d411d4050
chieger <charlie@codepath.com>
2018-06-02 15:25:31 -0700

    Return alphanumeric lowercase string from input

--- valid_string_palindrome.playground/Contents.swift
+++ valid_string_palindrome.playground/Contents.swift
@@ -2,4 +2,5 @@
 
 import UIKit
 
-var str = "Hello, playground"
+let myString = "A man, a plan, a canal: Panama"
+let alphaNumericString = myString.components(separatedBy: CharacterSet.alphanumerics.inverted).joined().lowercased()
